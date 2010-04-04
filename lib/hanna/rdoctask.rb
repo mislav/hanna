86cfb2b5ebbbbe165088742620fc9e33fa6db8ff
Mislav Marohnić <mislav.marohnic@gmail.com>
2010-04-04 17:33:21 +0200

    deprecate 'hanna/rdoctask' (use 'rake/rdoctask' instead)

--- lib/hanna/rdoctask.rb
+++ lib/hanna/rdoctask.rb
@@ -1,39 +1,16 @@
-require 'rake'
 require 'rake/rdoctask'
 
 Rake::RDocTask.class_eval do
-  # don't allow it
-  undef :external=, :template=
+  alias initialize_without_hanna initialize
   
-  # Create the tasks defined by this task lib.
-  def define # :nodoc:
-    options << '--format=hanna'
-    options << '--charset=UTF-8' if options.grep(/^(--charset\b|-c\b)/).empty?
+  def initialize(*args, &block)
+    initialize_without_hanna(*args, &block)
+    self.rdoc_dir = 'doc' if rdoc_dir == 'html'
     
-    desc "Build the HTML documentation"
-    task name
-    
-    desc "Force a rebuild of the RDOC files"
-    task paste("re", name) => [paste("clobber_", name), name]
-
-    desc "Remove rdoc products" 
-    task paste("clobber_", name) do
-      rm_r rdoc_dir rescue nil
+    before_running_rdoc do
+      require 'rdoc'
+      warn "Warning: requiring 'hanna/rdoctask' is deprecated. Use 'rake/rdoctask' instead."
+      self.options << '--format' << 'hanna'
     end
-
-    task :clobber => [paste("clobber_", name)]
-      
-    directory @rdoc_dir
-    task name => [rdoc_target]
-    file rdoc_target => @rdoc_files + [Rake.application.rakefile] do
-      require 'hanna/version'
-      Hanna::activate_rdoc_gem
-      require 'hanna'
-      
-      options = option_list + @rdoc_files
-      options.delete('--inline-source')
-      RDoc::RDoc.new.document(options)
-    end
-    return self
   end
 end
