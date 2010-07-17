0dd8f401e9fe7f2ccf08134f789420253ac22edb
Mislav Marohnić <mislav.marohnic@gmail.com>
2010-07-17 21:43:55 +0200

    rdoc hook does proper gem activation when Hanna is installed

--- lib/rdoc/discover.rb
+++ lib/rdoc/discover.rb
@@ -1,6 +1,12 @@
-# this file is auto-loaded from Hanna gem by RDoc
+# this file is auto-loaded by RDoc
 unless defined?(::Hanna)
   libdir = File.expand_path('../..', __FILE__)
-  $:.unshift(libdir) unless $:.include?(libdir)
+  unless $LOAD_PATH.include?(libdir)
+    # found in installed gems
+    name = File.basename(File.dirname(libdir))
+    version = name.sub!(/-([^-]+)$/, '') && $1
+    # activate self
+    gem name, version
+  end
   require 'hanna'
 end
